9809e0205ad7cd67b34c05e3b9a43688cad9eba5
cleiton-negreiros <cleiton.negreiros@gmail.com>
2025-04-23 11:17:58 -0300

    DB SQLite added.

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+import sqlite from 'sqlite';
+
+const db = new sqlite3.Database('library_db.sqlite', (err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err.message);
+    } else {
+        console.log('Conexão com o banco de dados estabelecida com sucesso.');
+}        
+});
+
+export default db;
